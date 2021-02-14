53597bc919644d40ff42ea9f060cb4d3c03f9b06
Slava <ucheba00@gmail.com>
2021-02-14 00:28:51 -0700

    add fetching post and single post

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+
+import axios from "axios";
 
 import BaseLayout from "../components/layout/BaseLayout";
 
-const Portfolio = () => {
+const Portfolio = (props) => {
+  const { posts } = props;
+
   return (
     <BaseLayout>
       <h1>Portfolio.js</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>
+            <Link as={`/portfolio/${post.id}`} href={`/portfolio/[id]`}>
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </BaseLayout>
   );
 };
+
+export async function getStaticProps() {
+  const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/`);
+  const posts = res.data;
+
+  return {
+    props: { posts: posts.slice(0, 10) },
+    revalidate: 1,
+  };
+}
 
 export default Portfolio;
