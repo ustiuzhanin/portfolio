da0a21de05f3b31939150a75c6eb137180a0933f
Slava <ucheba00@gmail.com>
2021-02-14 12:55:34 -0700

    add reactstrap and fonts

--- components/layout/BaseLayout.js
+++ components/layout/BaseLayout.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import Header from "../shared/Header";
+import { Container } from "reactstrap";
 
 const BaseLayout = (props) => {
   return (
-    <>
+    <Container>
       <Header />
       {props.children}
-    </>
+    </Container>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import BaseLayout from "../components/layout/BaseLayout";
+import { Button } from "reactstrap";
 
 const Index = () => {
   return (
     <BaseLayout>
       <h1>Index.js</h1>
+      <Button color='danger'>Danger</Button>
     </BaseLayout>
   );
 };
