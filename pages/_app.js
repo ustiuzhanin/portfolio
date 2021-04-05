c993a8a871ab1bf8ad82389ec27f1b26d2072071
Slava <ucheba00@gmail.com>
2021-04-04 22:04:20 -0600

    init project

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/main.scss";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
